c18cfff17c2d23505639ee82302bbff230348519
allseen <allseen@live.ru>
2023-02-16 15:38:21 +0300

    ok

--- 06.Tasks/32.py
+++ 06.Tasks/32.py
@@ -7,4 +7,3 @@ from random import randint
 start, stop = [int(i) for i in input("Enter range: ").split()]
 list_num = [randint(-10, 10) for i in range(randint(10, 20))]
 print(list_num, [i for i,n in enumerate(list_num) if n in range(start, stop+1)], sep="\n")
-
